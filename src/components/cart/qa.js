28eeb230486624f6413851769a1e1e3fe8d72aec
federicowoodward <83924684+federicowoodward@users.noreply.github.com>
2022-06-06 14:51:31 -0300

    QA + 10

--- src/components/cart/qa.js
+++ src/components/cart/qa.js
@@ -4,8 +4,10 @@ import "./qa.css";
 export default function QA() {
     const { photosQuantityAdedd } = UseCartContext();
     return (
-        <div className="numberQA">
-            { photosQuantityAdedd === 0 ? null : <span>{photosQuantityAdedd}</span> }
+        <div >
+            { photosQuantityAdedd === 0 ? null : photosQuantityAdedd > 9 ?
+            <span className="numberQA10">{photosQuantityAdedd}</span> :
+            <span className="numberQA">{photosQuantityAdedd}</span> }
         </div>
     );
 }
