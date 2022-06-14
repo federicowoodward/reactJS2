868904581a0db5c1704159a2092caf210158a781
federicowoodward <83924684+federicowoodward@users.noreply.github.com>
2022-06-14 13:43:46 -0300

    err fixed

--- src/assets/CartPhoto.js
+++ src/assets/CartPhoto.js
@@ -1,4 +1,2 @@
-import "./CartPhoto.css";    
-// import Image from "./carritoShop.png";
 export default function CartPhoto() { 
     return (<img src= "https://icongr.am/clarity/bookmark.svg?size=35&color=fefbfb" alt="Imagen de carrito" className="cartImg" ></img>  );}
